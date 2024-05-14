1a9bea67cba85b7f0d6b3255c5d9df37f23acb82
Xushnud Egamberdiyev <140577111+Xushnud-Egamberdiyev@users.noreply.github.com>
2024-05-14 10:44:27 +0500

    model inplement

---
+++ Rumassa.Backend/Rumassa.Aplication/Abstraction/IRumassaDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Rumassa.Domain.Entities;
+using Rumassa.Domain.Entities.Auth;
+namespace Rumassa.Aplication.Abstraction
+{
+    public interface IRumassaDbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Coupon> Coupons { get; set; }
+        public DbSet<Delivery> Deliveries { get; set; }
+        public DbSet<News> News { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<User> Users { get; set; }
+        public Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
